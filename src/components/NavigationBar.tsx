9f8feb2a4f06f5538ee946ec4ff712d587e08df5
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-02-21 17:20:38 -0300

    Changing main color style

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,14 +5,14 @@ import { MdOutlineComputer, MdOutlineMenu, MdClose } from "react-icons/md";
 
 export default function NavigationBar() {
   return (
-    <Disclosure as="nav" className="bg-white shadow">
+    <Disclosure as="nav" className="bg-dark shadow text-white">
       {({ open }:any) => (
         <>
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-200">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <MdClose className="block h-6 w-6" aria-hidden="true" />
@@ -37,7 +37,7 @@ export default function NavigationBar() {
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                   <a
                     href="#"
-                    className="border-indigo-500 text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    className="border-green-200 text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     Home
                   </a>
@@ -76,7 +76,7 @@ export default function NavigationBar() {
               <Disclosure.Button
                 as="a"
                 href="#"
-                className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+                className="bg-dark-gray border-green-200 text-green-200 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
                 Home
               </Disclosure.Button>
