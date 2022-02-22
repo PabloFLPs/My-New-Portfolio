252bebe0e5b621a4babc2aaadd50cb4d05cb517b
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-02-22 18:34:56 -0300

    Importing Projects section inside the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import NavigationBar from "../src/components/NavigationBar"
 import Hero from "../src/components/Hero"
+import Projects from "../src/components/Projects"
 
 function App() {
   return (
     <body>
       <NavigationBar/>
       <Hero/>
+      <Projects/>
     </body>
   )
 }
