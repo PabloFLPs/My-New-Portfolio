17362018649db14fea337a14c2742b4ddfa6e80d
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-02-22 19:29:23 -0300

    Importing Footer section inside the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react"
 import NavigationBar from "../src/components/NavigationBar"
 import Hero from "../src/components/Hero"
+import Footer from "../src/components/Footer"
 
 function App() {
   return (
     <body>
       <NavigationBar/>
       <Hero/>
+      <Footer/>
     </body>
   )
 }
