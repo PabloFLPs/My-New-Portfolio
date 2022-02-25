2e826ec1478df75f862fc03b85789994f96a5cdf
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-02-25 14:16:44 -0300

    Removing onClick function

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -41,7 +41,6 @@ export default function NavigationBar() {
                   {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                   <a
                     href="#"
-                    onClick={() => "window.scrollTo(0,0)"}
                     className="border-green-200 text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
                     Home
@@ -75,7 +74,6 @@ export default function NavigationBar() {
               <Disclosure.Button
                 as="a"
                 href="#"
-                onClick={() => "window.scrollTo(0,0)"}
                 className="bg-dark-gray border-green-200 text-green-200 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
               >
                 Home
