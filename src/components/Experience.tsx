1cf7698940abea8dce16c4ae3ee9aa44c5260031
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-03 19:37:01 -0300

    Changing title spacement

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -62,39 +62,37 @@ const experiences = [
 export default function Experiences() {
   return (
     <div id="Experience" className="bg-gray-900 text-white">
-      <div className="mx-auto pt-20 px-4 max-w-7xl sm:px-6 lg:px-8">
-        <div className="space-y-12">
-          <div className="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-            <h2 className="text-3xl font-bold text-white tracking-tight">My <span className="text-light-brown">Journey</span></h2>
-            <p className="text-xl text-gray-300">
-              Here you can check my academic profile and professional history:
-            </p>
-          </div>
-          <ul role="list" className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3">
-            {experiences.map((experience) => (
-              <li key={experience.role} className="px-14 py-6 bg-gray-800 text-center rounded-lg xl:text-left transition-all duration-300">
-                <a href={experience.linkedinUrl} target="_blank" className="text-gray-400">
-                  <div className="space-y-6">
-                    <div className="group xl:flex xl:items-center xl:justify-between space-y-4 xl:space-y-0 ">
-                      <experience.socialIcon className="m-auto h-12 w-12 xl:m-0 xl:w-10 xl:h-10"/>
-                      <div className="font-medium text-lg leading-6 xl:space-y-1 px-4">
-                        <h3 className="text-white">{experience.role}</h3>
-                        <p className="text-white">{experience.description}</p>
-                        <p className="text-light-brown">{experience.institution}</p>
-                      </div>
-                      <ul role="list" className="flex justify-center space-x-5">
-                        <li>
-                          <span className="sr-only">LinkedIn</span>
-                          <LinkedIn className="w-5 h-6 group-hover:text-light-brown"/>
-                        </li>
-                      </ul>
+      <div className="space-y-5 pt-20 pb-12 sm:space-y-4  px-14 md:px-24 lg:px-40 xl:px-40">
+        <h2 className="text-3xl font-bold text-white tracking-tight">My <span className="text-light-brown">Journey</span></h2>
+        <p className="text-xl text-gray-300">
+          Here you can check my academic profile and professional history:
+        </p>
+      </div>
+      <div className="mx-auto max-w-7xl">
+        <ul role="list" className="space-y-4 sm:grid sm:grid-cols-2 sm:gap-6 sm:space-y-0 lg:grid-cols-3">
+          {experiences.map((experience) => (
+            <li key={experience.role} className="px-14 py-6 bg-gray-800 text-center rounded-lg xl:text-left transition-all duration-300">
+              <a href={experience.linkedinUrl} target="_blank" className="text-gray-400">
+                <div className="space-y-6">
+                  <div className="group xl:flex xl:items-center xl:justify-between space-y-4 xl:space-y-0 ">
+                    <experience.socialIcon className="m-auto h-12 w-12 xl:m-0 xl:w-10 xl:h-10"/>
+                    <div className="font-medium text-lg leading-6 xl:space-y-1 px-4">
+                      <h3 className="text-white">{experience.role}</h3>
+                      <p className="text-white">{experience.description}</p>
+                      <p className="text-light-brown">{experience.institution}</p>
                     </div>
+                    <ul role="list" className="flex justify-center space-x-5">
+                      <li>
+                        <span className="sr-only">LinkedIn</span>
+                        <LinkedIn className="w-5 h-6 group-hover:text-light-brown"/>
+                      </li>
+                    </ul>
                   </div>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
+                </div>
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
