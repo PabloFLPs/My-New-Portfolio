b523f54e6611cf062489cbb78f608745323730a9
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-06-03 17:17:59 -0300

    Updating my resume

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -162,7 +162,7 @@ export default function AboutMe() {
                                 <p>
                                     I am always very curious about innovations and my knowledge in the technology and development areas. When something interests me, I seek to increase my knowledge about that.
                                     <p className="pt-4">
-                                        You can check my resume <a href="https://drive.google.com/file/d/185LM0OAUKOe1c2b2eCE9613QH37Pt_gb/view?usp=sharing" target="_blank" className="text-light-brown hover:opacity-80">here.</a>
+                                        You can check my resume <a href="https://drive.google.com/file/d/1YDfsMaXF1u2e7jKJnQ15ApQzBDNVcixk/view?usp=sharing" target="_blank" className="text-light-brown hover:opacity-80">here.</a>
                                     </p>
                                 </p>
                             </div>
